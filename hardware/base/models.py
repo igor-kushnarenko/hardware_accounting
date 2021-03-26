96e976cb148b01d6c5ff58ea7c23744dd8e709d4
igor <anapa.igor@yandex.ru>
2021-03-26 16:49:43 +0300

    - добавлены модели оборудования;

--- hardware/base/models.py
+++ hardware/base/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Hardware(models.Model):
+    manufacturer = models.ForeignKey(
+        'Manufacturer',
+        on_delete=models.SET_DEFAULT,
+        default=None,
+        null=True,
+        verbose_name='производитель',
+        related_name='hardware'
+    )
+    type = models.ForeignKey(
+        'Type',
+        on_delete=models.SET_DEFAULT,
+        default=None,
+        null=True,
+        verbose_name='тип',
+        related_name='hardware'
+    )
+    model = models.CharField(max_length=50, verbose_name='модель')
+    serial = models.CharField(max_length=50, verbose_name='серийный номер')
+    place = models.ForeignKey(
+        'Place',
+        on_delete=models.SET_DEFAULT,
+        default=None,
+        null=True,
+        verbose_name='расположение',
+        related_name='hardware'
+    )
+    status = models.ForeignKey(
+        'Status',
+        on_delete=models.SET_DEFAULT,
+        default=None,
+        null=True,
+        verbose_name='состояние',
+        related_name='hardware'
+    )
+    comment = models.TextField(max_length=2000, verbose_name='Примечание')
+
+    def __str__(self):
+        return self.model
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, verbose_name='состояние')
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name

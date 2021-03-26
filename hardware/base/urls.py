f8cae9f994df16c82a00d5a4bba59797ab548519
igor <anapa.igor@yandex.ru>
2021-03-26 15:46:21 +0300

    - сделал шаблон главной страницы;

--- hardware/base/urls.py
+++ hardware/base/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from .views import HelloView
+from . import views
 
 urlpatterns = [
-    path('', HelloView.as_view(), name='hello')
+    path('index/', views.index_view, name='index')
 ]
--- hardware/base/views.py
+++ hardware/base/views.py
@@ -1,11 +1,6 @@
-from django.views import View
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-class HelloView(View):
-    def get(self, request, *args, **kwargs):
-        todo_list = [
-            'Hello',
-        ]
-        return HttpResponse(
-            f"<ul><li>{todo_list[0]} </li></ul>'")
+def index_view(request):
+    return render(request, 'base/index.html', {})
+
